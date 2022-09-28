7e32f317541f1e14ea1be9292bace1d4b1746203
jetmaax <p.maxime62@gmail.com>
2022-09-28 14:02:48 +0200

    first

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $products = Product::simplePaginate (2);
+        return view('index', compact ('produits, $produits'));
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $product = Product::create([
+            'name' => $request->name,
+            'color' => $request->color,
+            'weight' => $request->weight,
+            'price' => $request->price,
+            'description' => $request->description,
+        ]);
+
+        return redirect()->back()->with('success', 'Product successfully stored.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $product = Product::findOrFail($id);
+
+        return view('show', compact('product', $product));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $product = Product::findOrFail($id);
+
+        return view('edit', compact('product', $product));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $product = Product::findOrFail($id);
+
+        $product->update([
+            'name' => $request->name,
+            'color' => $request->color,
+            'weight' => $request->weight,
+            'price' => $request->price,
+            'description' => $request->description,
+        ]);
+
+        return redirect()->back()->with('success', 'Product successfully updated.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $product = Product::findOrFail($id);
+
+        $product->delete();
+
+        return redirect()->back()->with('success', 'Product successfully deleted.');
+    }
+}
+
+
